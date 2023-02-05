78122a5fbab3f7c593d52e85cf44966908c70879
voynow <voynow99@gmail.com>
2023-02-05 17:44:32 -0500

    First Generated tweet, 'It's alive...' - Frankenstein

--- utils/openai.py
+++ utils/openai.py
@@ -3,5 +3,28 @@ import openai
 from openai import Completion
 import utils.secrets_manager as secrets_manager
 
-# Set the API key
 openai.api_key = secrets_manager.get_secrets()['openai_secret_key']
+
+
+def gpt_completion(
+    model_engine,
+    prompt,
+    max_tokens=45,
+    temperature=1.0,
+    frequency_penalty=0.5,
+    presence_penalty=0.5,
+    n=1,
+    stop=None,
+):
+
+    completions = openai.Completion.create(
+        engine=model_engine,
+        prompt=prompt,
+        max_tokens=max_tokens,
+        temperature=temperature,
+        frequency_penalty=frequency_penalty, 
+        presence_penalty=presence_penalty,
+        n=n,
+        stop=stop,
+    )
+    return completions.choices[0].text
