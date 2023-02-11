214caf35642de41b49e18f4efcd5ec693c6e8901
voynow <voynow99@gmail.com>
2023-02-11 08:38:12 -0500

    Code from tweepy and openai utilities

---
+++ tweet-lambda/twitter.py
@@ -0,0 +1,28 @@
+
+import secrets_manager
+import tweepy
+
+secrets = secrets_manager.get_secrets()
+
+
+def get_auth():
+    """
+    """
+    auth = tweepy.OAuthHandler(
+        secrets['api_key'], 
+        secrets['api_key_secret'])
+
+    auth.set_access_token(
+        secrets['access_token'], 
+        secrets['access_token_secret'])
+
+    return tweepy.API(
+        auth=auth,
+        wait_on_rate_limit=True)
+
+
+def tweet(message):
+    """
+    """
+    api = get_auth()
+    return api.update_status(message)
