765d27ead2374f02b541211a1af99163a9e4f66d
voynow <voynow99@gmail.com>
2023-02-25 10:43:33 -0500

    Upgrading trends list

--- tweet_lambda/utils/trends.py
+++ tweet_lambda/utils/trends.py
@@ -57,10 +57,60 @@ trends = [
     "Ethical AI",
     "Machine Learning Ops (MLOps)",
     "Autonomous IoT Devices",
-    "Fog Computing",
     "Software-Defined Networking (SDN)",
-    "Swarm Intelligence"
+    "Swarm Intelligence",
+    'mental health and well-being',
+    'remote work and distributed teams',
+    'growth of e-commerce and online shopping',
+    'renewable energy and sustainable practices',
+    'alternative currencies and payment methods',
+    'privacy and data protection',
+    'adoption of electric and autonomous vehicles',
+    'mindfulness and meditation',
+    'AI personalized platforms',
+    'community building and social connections',
+    'social responsibility and ethical business',
+    'lifelong learning and professional development',
+    'remote education and digital learning platforms',
+    'eco-friendly and sustainable fashion and consumer goods',
+    'demand for cloud computing',
+    'adoption of DevOps methodologies in all industries',
+    'data analysis and visualization skills',
+    'agile development and continuous delivery',
+    'soft skills, communication and collaboration',
+    'Personalized content recommendation systems',  # AI and ML
+    'Voice-enabled virtual assistants',  # Natural Language Processing
+    'Decentralized digital identity management',  # Blockchain
+    'Peer-to-peer payment system',  # Cryptocurrency
+    'Real-time predictive maintenance for industrial equipment',  # Edge Computing
+    'Cloud-based software development and deployment',  # Cloud Computing
+    'Autonomous vehicles and smart transportation systems',  # 5G Technology
+    'Advanced threat detection and response',  # Cybersecurity
+    'Drug discovery and development',  # Quantum Computing
+    'Immersive training simulations',  # Virtual Reality
+    'Enhanced customer experience in retail',  # Augmented Reality
+    'Predictive maintenance in manufacturing',  # Big Data
+    'Rapid and continuous software delivery',  # Cloud-native Computing
+    'Event-driven processing for IoT devices',  # Serverless Computing
+    'Privacy-preserving machine learning models',  # Federated Learning
+    'Self-driving ride-sharing services',  # Autonomous Vehicles
+    'Conversational customer support',  # Chatbots
+    'Human-robot collaboration in manufacturing',  # Collaborative Robots
+    'Real-time object recognition in surveillance systems',  # Computer Vision
+    'Interactive dashboards for data analysis',  # Data Visualization
+    'Connected devices and services in smart homes',  # Digital Ecosystems
+    'Automated aerial inspection and monitoring'  # Drones
+    "colonization of other planets such as Mars and the Moon",
+    "mining of asteroids and other celestial bodies for resources",
+    "The future and large space habitats",
+    "advanced propulsion technologies for beter space travel",
+    "robotic missions to explore and study distant space",
+    "advanced life support systems to sustain human life in space",
+    "establishing communication networks across the solar system",
+    "The future of spacecrafts that can travel through interstellar space",
+    "studying extraterrestrial life and its impact on humans"
 ]
+
 
 
 def get_trend(count=1):
